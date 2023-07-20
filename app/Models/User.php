136b42194bb57f5d3a0fabd3f7087085aa694166
Lucas Moreyra <lucasmoreyra1703@gmail.com>
2023-07-20 00:42:56 -0300

    agregado tipo en

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'tipo',
     ];
 
     /**
