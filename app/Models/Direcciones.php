74015f930834be3e2b3a4078a6fc76efaddb9698
Lucas Moreyra <lucasmoreyra1703@gmail.com>
2023-07-23 02:45:16 -0300

    modelo de direcciones

---
+++ app/Models/Direcciones.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Direcciones extends Model
+{
+    use HasFactory;
+    protected $table = 'direcciones';
+    protected $primaryKey = 'idDireccion';
+    protected $fillable = [
+        'idRuta', 'direccion', 'latitud', 'longitud', 'tipo', 'orden'
+    ];
+
+    // Si no deseas utilizar los timestamps 'created_at' y 'updated_at', establece el siguiente atributo a false
+    public $timestamps = true;
+
+    // Relación con el modelo 'Ruta'
+    public function ruta()
+    {
+        return $this->belongsTo(Ruta::class, 'idRuta', 'idRuta');
+    }
+}
