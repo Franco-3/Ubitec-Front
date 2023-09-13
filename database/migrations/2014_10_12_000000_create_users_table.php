d39d7d6dae62f036de84fc368f37a17773bf09fa
Lucas Moreyra <lucasmoreyra1703@gmail.com>
2023-09-13 16:48:28 -0300

    Migracion modificada: campo empresa agregado

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('tipo', 30);
+            $table->string('empresa', 100)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
